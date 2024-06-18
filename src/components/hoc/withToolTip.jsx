7725dae960886475d242af3663487e3f9883b0fe
ZorroLeVrai <69362591+ZorroLeVrai@users.noreply.github.com>
2024-06-18 15:35:58 +0200

    paramètre par défaut

--- src/components/hoc/withToolTip.jsx
+++ src/components/hoc/withToolTip.jsx
@@ -1,15 +1,14 @@
 import React, { useState } from 'react';
 
-const withToolTip = (Component, toolTipText) => {
+const withToolTip = (Component, toolTipText = "Some tooltip") => {
     const EnhanceComponent = (props) => {
         const [showTooltip, setShowTooltip] = useState(false);
-        const toolTipContent = toolTipText ?? "Some tooltip";
         const mouseOver = () => setShowTooltip(true);
         const mouseOut = () => setShowTooltip(false);
         return (
         <>
             <span onMouseOver={mouseOver} onMouseOut={mouseOut}><Component {...props} /></span>
-            {showTooltip && <div><span className="border">{toolTipContent}</span></div>}
+            {showTooltip && <div><span className="border">{toolTipText}</span></div>}
         </>
         );
     };
