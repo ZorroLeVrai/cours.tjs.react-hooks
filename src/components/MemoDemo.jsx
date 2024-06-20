cd34ffcae61252a406fa24ea6e456477dbd2eb2c
ZorroLeVrai <69362591+ZorroLeVrai@users.noreply.github.com>
2024-06-20 04:58:08 +0200

    Mise à jour MemoDemo

--- src/components/MemoDemo.jsx
+++ src/components/MemoDemo.jsx
@@ -1,8 +1,8 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import { useState, useEffect } from 'react';
 import styles from "./Global.module.css";
 
-const Child = ({componentName, value, callback}) => {
+const Child = ({componentName, value}) => {
   useEffect(() => console.log(`${componentName} re-rendered val:${value}`));
 
   return (
@@ -18,13 +18,18 @@ const ChildMemo = React.memo(Child);
 export default function MemoDemo() {
   const [fakeState, setFakeState] = useState(false);
 
-  const cbFunc = useCallback(() => {}, []);
+  const maFonction = () => {}
+  const cbFunc = useCallback(maFonction, []);
+
+  const x = 12;
+  //const obj = {x};
+  const obj = useMemo(()=>({ x }), [x]);
 
   return (
     <div className={styles.border}>
       <button onClick={() => setFakeState(current => !current)}>{fakeState ? "true":"false"}</button>
-      <Child componentName={"Child"} value={42} />
-      <ChildMemo componentName={"ChildMemo"} value={42} callback={cbFunc} />
+      <Child componentName={"Child"} value={42} callback={maFonction} />
+      <ChildMemo componentName={"ChildMemo"} value={42} callback={maFonction} />
     </div>
   );
 }
